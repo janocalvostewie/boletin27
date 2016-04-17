5fd4f0d73a4edcbc972b3b35f8dcd652b12999df
jano <jano@JANO-PC.LOGISTICA_VIGO>
2016-04-17 22:58:45 +0200

    boletin27

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,18 @@
+package boletin27;
+
+/**
+ *
+ * @author jano
+ */
+public class Boletin27 {
+    public static void main(String[] args) {
+        
+        Menu menu1=new Menu();
+        
+        menu1.setVisible(true);
+        
+        
+        
+    }
+    
+}
